8e91555ca16a41037fb70aa5f3fa845d59e27c23
Dan Steren <daniel.d.steren@gmail.com>
2017-03-18 18:41:15 -0600

    Integrate challenge creation scene into app

--- app/components/Icons/ExpirationIcon.js
+++ app/components/Icons/ExpirationIcon.js
@@ -10,8 +10,8 @@ export default class ExpirationIcon extends Component {
         <Svg width={this.props.size || 24} height={this.props.size || 24}>
           <Path
             scale={scale}
-            d="M9.5 20l2.5-2.5 2.5 2.5zM6 2v6h.01L6 8.01 10 12l-4 4 .01.01H6V22h12v-5.99h-.01L18 16l-4-4 4-3.99-.01-.01H18V2H6zm10 14.5V20H8v-3.5l4-4 4 4zm-4-5l-4-4V4h8v3.5l-4 4zm-2.5-4L12 10l2.5-2.5z"
-            fill="#000"
+            d='M9.5 20l2.5-2.5 2.5 2.5zM6 2v6h.01L6 8.01 10 12l-4 4 .01.01H6V22h12v-5.99h-.01L18 16l-4-4 4-3.99-.01-.01H18V2H6zm10 14.5V20H8v-3.5l4-4 4 4zm-4-5l-4-4V4h8v3.5l-4 4zm-2.5-4L12 10l2.5-2.5z'
+            fill='#000'
             fillOpacity='.50'
           />
         </Svg>
--- app/containers/scenes/ChallengeCreationScene.js
+++ app/containers/scenes/ChallengeCreationScene.js
@@ -1,32 +1,76 @@
-import React, { Component } from 'react';
-import {
-  View,
-  Text,
-  Button
-} from 'react-native';
+import React, { Component, PropTypes } from 'react';
+import { View, Text, TextInput, ScrollView, Button } from 'react-native';
 
+import { ChallengeCreationCSS as Css } from './ChallengeCreationScene.css'
+import { DescriptionInput, ExpirationInput, ListDivider, LocationsList } from '../../components';
 import { LocationCreationRoute } from '../../routes/defaultRoutes'
 
 export default class ChallengeCreationScene extends Component {
-
   constructor(props) {
     super(props);
     this.goToLocationCreationScene = this.goToLocationCreationScene.bind(this);
+    if (this.props.challenge) {
+      this.state = {
+        title: this.props.challenge.title,
+        description: this.props.challenge.description,
+        expiration: this.props.challenge.expiration,
+        locations: this.props.challenge.locations
+      };
+    } else {
+      this.state = {
+        title: '',
+        description: '',
+        expiration: undefined,
+        locations: []
+      }
+    }
+  }
+
+  render() {
+    return (
+      <View style={Css.page}>
+        <TextInput
+          style={Css.challengeTitle}
+          autoCapitalize='sentences'
+          placeholderTextColor='#FFFFFFDE'
+          underlineColorAndroid='transparent'
+          onChangeText={(title) => this.setState({ title })}
+          value={this.state.title}
+          placeholder='Enter a title for your challenge'
+        />
+        <ScrollView>
+          <View style={{height: 8}}/>
+          <ExpirationInput
+            expiration={this.state.expiration}
+            onChange={(expiration) => this.setState({ expiration })}
+          />
+          <ListDivider/>
+          <DescriptionInput
+            description={this.state.description}
+            onChange={(description) => this.setState({ description })}
+          />
+          <View style={{height: 8}}/>
+          <LocationsList
+            locations={this.state.locations}
+            onChange={(locations) => this.setState({ locations })}
+          />
+        </ScrollView>
+        <Button
+          onPress={this.goToLocationCreationScene}
+          title="Go To Location Creation Scene"
+        />
+        <View style={Css.footer}>
+          <Text style={Css.submitButton}>SUBMIT</Text>
+        </View>
+      </View>
+    )
   }
 
   goToLocationCreationScene() {
     this.props.navigator.push(LocationCreationRoute);
   }
-
-  render() {
-    return (
-      <View>
-        <Text style={{textAlign:"center"}}>[Challenge Creation Scene]</Text>
-        <Button 
-          onPress={this.goToLocationCreationScene}
-          title="Go To Location Creation Scene"
-          />
-      </View>
-    );
-  }
 }
+
+ChallengeCreationScene.propTypes = {
+  challenge: PropTypes.object
+};
