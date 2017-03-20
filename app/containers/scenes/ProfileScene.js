1d8f625f275a995b7c3020485b098348a3f47dfe
athompson909 <athompson909@gmail.com>
2017-03-20 17:00:08 -0600

    separated styles

--- app/containers/scenes/ProfileScene.js
+++ app/containers/scenes/ProfileScene.js
@@ -1,13 +1,13 @@
-
 import React, { Component } from 'react';
 import {
-  StyleSheet,
   View,
   Text,
   TextInput,
   Image,
   TouchableHighlight
 } from 'react-native';
+
+import { ProfileSceneCss as styles } from './ProfileScene.css.js'
 
 export default class ProfileScene extends Component {
 
@@ -88,76 +88,3 @@ export default class ProfileScene extends Component {
     )
   }
 }
-
-
-const styles = StyleSheet.create({
-  icon: {
-    alignSelf: 'center',
-    height: 120,
-    width: 120,
-    borderRadius: 60,
-    marginTop: -60,
-    backgroundColor: 'grey',
-    zIndex: 2,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  backgroundImage: {
-    height: 230, backgroundColor: 'steelblue', zIndex: 1
-  },
-  profPic: {
-    color: '#fff',
-    fontSize: 68,
-  },
-  bottom: {
-    flex:1,
-    backgroundColor:'#ddd'
-  },
-  bottomText: {
-    // flex:1, // this makes everything stay in the middle
-    marginTop: 20,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  displayName: {
-    fontSize: 34,
-    fontWeight: 'bold',
-    padding: 6,
-  },
-  displayNameEdit: {
-    fontSize: 34,
-    height: 42,
-    padding: 6,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#eee',
-    marginLeft: 32,
-    fontWeight: 'bold',
-    marginTop: 6,
-    marginRight: 32,
-    marginBottom: 12
-  },
-  textBelow: {
-    fontSize: 20,
-    padding: 3
-  },
-  editIconTH: {
-    width: 26,
-    height: 26,
-    marginLeft: 12,
-    marginTop: 6
-  },
-  editIcon: {
-    width: 30,
-    height: 30
-  },
-  cancelEditIconTH: {
-    width: 26,
-    height: 26,
-    marginLeft: 12,
-    alignSelf: 'flex-end'
-  },
-  cancelEditIcon: {
-    width: 30,
-    height: 30  }
-})
