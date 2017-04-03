f3ce5cb4d2a8a9e0f4666ac7050a2d52f0b8b313
athompson909 <athompson909@gmail.com>
2017-04-03 13:48:36 -0600

    login works but not sufficiently tested

--- app/containers/scenes/LoginScene.js
+++ app/containers/scenes/LoginScene.js
@@ -7,7 +7,8 @@ import {
   Image,
   TouchableHighlight,
   Alert,
-  Button
+  Button,
+  AsyncStorage
 } from 'react-native';
 
 var Auth0Lock = require('react-native-lock');
@@ -18,12 +19,18 @@ import { MainMapRoute } from '../../routes/defaultRoutes';
 export default class LoginScene extends Component {
 
   constructor(props) {
+
     super(props);
-    this.showLoginScreen();
+  }
+
+  componentDidMount() {
+
+    this.checkIfLoggedIn();
   }
 
   showLoginScreen() {
 
+    // the options set makes it impossible to create an account unless you use google or facebook
     lock.show({
       disableSignUp: true,
       disableResetPassword: true,
@@ -35,18 +42,76 @@ export default class LoginScene extends Component {
         console.error(err);
         return;
       }
+      let response = this.createNewUser(profile.name, profile.userId);
 
-      console.log('success?');
-      console.log(profile);
-      console.log(token);
+      if(!response.success) switch (response.error) {
+        case 'duplicate_surrogateId':
+          // user already exists
+          // get user's id
+          console.log('user already exists');
+          this.storeData(profile.name, profile.userId, undefined);
+          break;
+        default:
+          break;
+      }
+      else {
+        this.storeData(profile.name, profile.userId, response.id);
+      }
 
 
-      // TODO: create new user
-      // TODO: use AsyncStorage to store user data
-
-      // go to main map scene when done
       this.goToMainMapScene();
     });
+  }
+
+  createNewUser(name, id) {
+    let formData = new FormData();
+    formData.append('token', 'geo-ninjas');
+    formData.append('name', name);
+    formData.append('surrogateId', id);
+    fetch('http://enexia.com:10000/geo-challenge/user/create', {
+      method: 'POST',
+      body: formData
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+      return responseJson;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  }
+
+  async storeData(name, surrogateId, id) {
+    try {
+
+      await AsyncStorage.setItem('username', name);
+      await AsyncStorage.setItem('surrogateId', surrogateId)
+      await AsyncStorage.setItem('id', id);
+    }
+    catch(err) {
+      console.log('can\'t save data');
+      console.error(err);
+    }
+  }
+
+  async checkIfLoggedIn() {
+    try {
+
+      const username = await AsyncStorage.getItem('username');
+      const id = await AsyncStorage.getItem('id');
+
+      if(username == undefined || id == undefined) {
+        this.showLoginScreen();
+      }
+      else {
+        this.goToMainMapScene();
+      }
+    }
+    catch(err) {
+      console.log(err);
+      this.showLoginScreen();
+    }
   }
 
   goToMainMapScene() {
